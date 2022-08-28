274abb7749798c3a9182fbeb487b501070ed8512
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2022-08-28 11:41:05 -0500

    Update EasySQL.sql

--- EasySQL.sql
+++ EasySQL.sql
@@ -3,3 +3,12 @@
 SELECT player_id, min(event_date) AS "first_login" 
 FROM Activity
 GROUP BY 1;
+
+
+#https://leetcode.com/problems/customer-placing-the-largest-number-of-orders/
+#SQL 586
+SELECT customer_number 
+FROM orders
+GROUP BY customer_number
+order by count(customer_number)
+DESC LIMIT 1;
