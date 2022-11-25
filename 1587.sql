7f8207d50f0ee87cd623e22e70de91cb416305dd
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2022-11-25 11:13:10 -0600

    Update 1587.sql

--- 1587.sql
+++ 1587.sql
@@ -1,3 +1,8 @@
+# Make new column balance with sum of transaction amounts
+# Merge two tables users/transactions through account
+# Make columns GROUPED through account 
+# GROUPBY must use having keyword cannot use where 
+
 # https://leetcode.com/problems/bank-account-summary-ii/solutions/2578691/bank-account-summary-ii-solution-sql/
 SELECT users.name, SUM(transactions.amount) as balance
 FROM users
